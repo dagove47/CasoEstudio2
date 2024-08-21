0020df4b13fc05ee9e09ade2c63748022aff9ce4
David Gómez <dagove47@gmail.com>
2024-08-20 23:52:28 -0600

    views added

---
+++ CasoEstudio2/Controllers/CasasController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasoEstudio2.Controllers
+{
+    public class CasasController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Consulta()
+        {
+            ViewBag.msj = "";
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Alquiler()
+        {
+            ViewBag.msj = "";
+            return View();
+        }
+
+        // [HttpPost]
+        // public ActionResult Alquiler()
+        // {
+        //     return View();
+        // }
+    }
+}
